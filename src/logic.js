e39bdb2942248394334aec2cc91705a564e6e595
PaingSuLt <pkhazel72@gmail.com>
2026-07-27 00:52:42 +0630

    refactor: separate weather data processing and clean up event listeners

--- src/logic.js
+++ src/logic.js
@@ -1,39 +1,63 @@
 import { API_KEY } from "./api-key.js";
 
-const icon = document.getElementById("icon");
 const searchBar = document.getElementById("search-bar");
 const searchBtn = document.getElementById("search-btn");
 
-async function Search(location) {
-  try {
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`,
-    );
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      icon.textContent = data.timezone + ": " + data.currentConditions.icon;
-      return data;
-    } else {
-      icon.textContent = `Error ${response.status}. Location Not found!`;
-    }
-  } catch (error) {
-    icon.textContent = "Network connection error!";
-    console.log(error);
+async function fetchData(location) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`,
+  );
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+}
+
+function processData(data) {
+  return {
+    address: data.resolvedAddress,
+    timeZone: data.timezone,
+    condition: data.currentConditions.conditions,
+    feelsLike: data.currentConditions.feelslike,
+    humidity: data.currentConditions.humidity,
+    temp: data.currentConditions.temp,
+    icon: data.currentConditions.icon,
+  };
+}
+
+async function handleSearch() {
+  const location = searchBar.value.trim();
+  if (!location) return;
+
+  let data = await fetchData(location);
+  console.log(data);
+
+  if (data) {
+    let weather = processData(data);
+    console.log(weather);
+    weatherCard(weather);
+    searchBar.value = "";
   }
 }
 
 export function init() {
-  searchBtn.addEventListener("click", () => {
-    console.log(searchBar.value);
-    Search(searchBar.value);
-    searchBar.value = "";
-  });
   searchBar.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      Search(searchBar.value);
-      console.log(searchBar.value);
-      searchBar.value = "";
-    }
+    if (e.key === "Enter") handleSearch();
   });
+
+  searchBtn.addEventListener("click", handleSearch);
+}
+
+function weatherCard(weather) {
+  const timeZone = document.getElementById("time-zone");
+  const iconImage = document.getElementById("icon-image");
+  const temperature = document.getElementById("temperature");
+  const feelsLike = document.getElementById("feels-like");
+  const weatherCondition = document.getElementById("weather");
+
+  timeZone.textContent = weather.timeZone;
+  //icon here
+  temperature.textContent = weather.temp + " F";
+  feelsLike.textContent = "Feels Like " + weather.feelsLike;
+  weatherCondition.textContent = weather.condition;
 }
